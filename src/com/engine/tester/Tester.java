70c3c67037bd0845235ece1df0cc37512a7f03bf
AlimWicaksono <wicayudha.wy@gmail.com>
2018-02-15 10:32:18 +0700

    Minnor Fix in class TexturedModel

--- src/com/engine/tester/Tester.java
+++ src/com/engine/tester/Tester.java
@@ -1,9 +1,12 @@
 package com.engine.tester;
 
-import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.*;
 
-import com.engine.models.RawModel;
+import com.engine.entities.*;
+import com.engine.models.*;
+import com.engine.objconverter.OBJFileLoader;
 import com.engine.render.*;
+import com.engine.texture.*;
 
 public class Tester {
 	
@@ -12,18 +15,25 @@ public class Tester {
 		
 		Renderer render = new Renderer();
 		Loader loader = new Loader();
-		
-		
+		Camera camera = new Camera();
 		
 		RawModel model = loader.loadToVao(null);	
 		
 		while(!Display.isCloseRequested()) {
+			
 			render.prepare();
 			render.render(model);
 			DisplayManager.update();			
 		}
 		
+		loader.cleanUp();
 		DisplayManager.destroy();
+	}
+	
+	private static TexturedModel loadModel(String fileName, Loader loader) {
+		RawModel model = OBJFileLoader.loadOBJ(fileName, loader);
+		ModelTexture texture = new ModelTexture(loader.loadTexture(fileName));
+		return new TexturedModel(model, texture);
 	}
 	
 }
